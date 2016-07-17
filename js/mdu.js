ed2a65fcdeeab9aea717ecfc04901f313d71637b
Martin Salazard <martin.der.aze@gmail.com>
2016-07-17 21:55:29 +0200

    mdu : add method to check is URL is absolute

--- js/mdu.js
+++ js/mdu.js
@@ -1,6 +1,13 @@
 
 var MDU = {};
 
+
+MDU.REGEX_isAbsoluteURL = new RegExp('^(?:[a-z]+:)?//', 'i');
+MDU.isAbsoluteURL = function(url) {	
+	return MDU.REGEX_isAbsoluteURL.test(url); 	
+}
+
+
 MDU.getURLParameter = function (key) {
 	return decodeURIComponent((new RegExp('[?|&]' + key + '=' + '([^&;]+?)(&|#|;|$)').exec(location.search) || [null, ''])[1].replace(/\+/g, '%20')) || null;
 };
