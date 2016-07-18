79eece02321146f0d7446e7c903fed2df4954b9f
Martin Salazard <martin.der.aze@gmail.com>
2016-07-18 23:20:27 +0200

    mdu : add function to find out location for any URL

--- js/mdu.js
+++ js/mdu.js
@@ -7,6 +7,13 @@ MDU.isAbsoluteURL = function(url) {
 	return MDU.REGEX_isAbsoluteURL.test(url); 	
 }
 
+MDU.getLocation = function(url) { 
+	var a = document.createElement("a"); 
+	a.href = url; 
+	var l = { protocol : a.protocol, hostname : a.hostname, port : a.port, pathname : a.pathname }; 
+	return l; 
+}
+
 
 MDU.getURLParameter = function (key) {
 	return decodeURIComponent((new RegExp('[?|&]' + key + '=' + '([^&;]+?)(&|#|;|$)').exec(location.search) || [null, ''])[1].replace(/\+/g, '%20')) || null;
