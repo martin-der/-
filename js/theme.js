462ed6501169ec9394950fb32a9a8511985d044c
Martin Salazard <martin.der.aze@gmail.com>
2016-07-27 22:55:34 +0200

    theme : now load with ajax

--- js/theme.js
+++ js/theme.js
@@ -5,7 +5,6 @@ Theme.config = {};
 
 Theme.config.configurator = {
 	themes : [],
-	default_theme : null,
 	selects_selector : "select#theme",
 	location_system : 'theme',
 	location : 'theme/{0}',
@@ -33,16 +32,15 @@ Theme.Configurator = Theme.Configurator || function(a) {
 	
 	var config = a ? jQuery.extend(true, {}, Theme.config.configurator, a ) : jQuery.extend(true, {}, Theme.config.configurator );
 
-
 	return {
 		config : config,
 		setup : function() {
 
 			var head = jQuery("html>head");
 
-			head.append(jQuery("<link>",{ id:'theme_reset_css', rel:'stylesheet'}));
-			head.append(jQuery("<link>",{ id:'theme_main_css', rel:'stylesheet'}));
-			head.append(jQuery("<link>",{ id:'theme_mandatory_css', rel:'stylesheet'}));
+			head.append(jQuery("<style>",{ id:'theme_reset_css', type:'text/css'}));
+			head.append(jQuery("<style>",{ id:'theme_main_css', type:'text/css'}));
+			head.append(jQuery("<style>",{ id:'theme_mandatory_css', type:'text/css'}));
 
 			var themeSelects = jQuery(config.selects_selector);
 
@@ -81,8 +79,9 @@ Theme.Configurator = Theme.Configurator || function(a) {
 						Cookies.set('theme', theme);
 					}
 				} catch (ex) {
-					if (this_configurator.post_process.fail)
-						this_configurator.post_process.fail(theme, ex);
+					if (console && console.error) console.error(ex);
+					if (this_configurator.config.post_process.fail)
+						this_configurator.config.post_process.fail(theme, ex);
 				} finally {
 					if (jQuery.blockUI) {
 						jQuery.unblockUI();
@@ -99,10 +98,6 @@ Theme.Configurator = Theme.Configurator || function(a) {
 			if (!name && Cookies) {
 				name = Cookies.get('theme');
 			}
-			
-			if (!name && this.config.default_theme) {
-				name = this.config.default_theme;
-			}
 
 			if ( name ) {
 				this.setTheme ( name );
@@ -111,6 +106,8 @@ Theme.Configurator = Theme.Configurator || function(a) {
 
 		},
 		setTheme : function ( name ) {
+			var urlPrefix = '';
+			
 			var i;
 			
 			var isExtra = false;
@@ -138,28 +135,77 @@ Theme.Configurator = Theme.Configurator || function(a) {
 				throw "No such theme '"+name+"'";
 			}
 			
-			var resetCssLink = "<link id='theme_reset_css' rel='stylesheet' href='"+this.config.location_system+"/reset.css' type='text/css'>";
-			
+			var cssUrl = {
+				reset : this.config.location_system+"/reset.css",
+				main : null,
+				mandatory : this.config.location_system+"/mandatory.css"
+			}
+
 			var mainUrl;
 			if (!theme.url) {
 				if (isExtra) {
-					mainUrl = MDU.string.format(this.config.extra.location, theme.name)+"/main.css";
+					cssUrl.main = MDU.string.format(this.config.extra.location, theme.name)+"/main.css";
 				} else {
-					mainUrl = MDU.string.format(this.config.location, theme.name)+"/main.css";
+					cssUrl.main = MDU.string.format(this.config.location, theme.name)+"/main.css";
 				}
 			} else {
-				mainUrl = MDU.string.format(this.config.location, theme.name);
+				cssUrl.main = theme.url+'/main.css';
 			}
-			var mainCssLink = "<link id='theme_main_css' rel='stylesheet' href='"+mainUrl+"' type='text/css'>";
+			
+			var css_files = {
+				reset : { name : 'reset', url : cssUrl.reset,loading : true, content : null, error : null },
+				main : { name : 'main', url : cssUrl.main,  loading : true, content : null, error : null },
+				mandatory : { name : 'mandatory', url : cssUrl.mandatory, loading : true, content : null, error : null }
+			}
+			
+			var jqXHR_done_handler_builder = function(css_file) {
+				return function(data, textStatus, jqXHR) {
+					var prefix = (css_file.url.indexOf('/') > -1) ? MDU.string.dirname(css_file.url) : "";
+					css_file.content = data.replace(/url\((['"]?)(.+?)\1\)/g, "url('"+prefix+"$2')");
+				};
+			};
+			var jqXHR_fail_handler_builder = function(css_file) {
+				return function(jqXHR, textStatus, errorThrown) {
+					css_file.error = errorThrown;
+				};
+			};
+			var jqXHR_always_handler_builder = function(css_file) {
+				return function() {
+					css_file.loading = false;
+					if (css_files.reset.loading === false && css_files.main.loading === false && css_files.mandatory.loading === false) {
+						if (css_files.reset.content !== null && css_files.main.content !== null && css_files.mandatory.content !== null) {
+							jQuery("head>style[id='theme_reset_css']").html(css_files.reset.content);
+							jQuery("head>style[id='theme_main_css']").html(css_files.main.content);
+							jQuery("head>style[id='theme_mandatory_css']").html(css_files.mandatory.content);
+							if (config.post_process.done) {
+								config.post_process.done(theme);
+							}
+						} else {
+							if (config.post_process.fail) {
+								var failures = [];
+								if (css_files.reset.error) failures.push(jQuery.extend(true, {name:'reset'}, css_files.reset));
+								if (css_files.main.error) failures.push(jQuery.extend(true, {name:'main'}, css_files.main));
+								if (css_files.mandatory.error) failures.push(jQuery.extend(true, {name:'mandatory'}, css_files.mandatory));
+								config.post_process.fail(theme,failures);
+							}
+						}
+					}
+				};
+			};
 
-			var mandatoryCssLink = "<link id='theme_mandatory_css' rel='stylesheet' href='"+this.config.location_system+"/mandatory.css' type='text/css'>";
+			var jqXHR = jQuery.get( cssUrl.reset )
+				.done(jqXHR_done_handler_builder(css_files.reset))
+				.fail(jqXHR_fail_handler_builder(css_files.reset))
+				.always(jqXHR_always_handler_builder(css_files.reset));
+			var jqXHR = jQuery.get( cssUrl.main )
+				.done(jqXHR_done_handler_builder(css_files.main))
+				.fail(jqXHR_fail_handler_builder(css_files.main))
+				.always(jqXHR_always_handler_builder(css_files.main));
+			var jqXHR = jQuery.get( cssUrl.mandatory )
+				.done(jqXHR_done_handler_builder(css_files.mandatory))
+				.fail(jqXHR_fail_handler_builder(css_files.mandatory))
+				.always(jqXHR_always_handler_builder(css_files.mandatory));
 
-			//jQuery.get(myStylesLocation, function(css)
-			//{
-			//	jQuery('<style type="text/css"></style>').html(css).appendTo("head");
-			//});
-			jQuery("html>head>link[id='theme_reset_css'],html>head>link[id='theme_main_css'],html>head>link[id='theme_mandatory_css']").remove();
-			jQuery("html>head").append(resetCssLink + mainCssLink + mandatoryCssLink);
 		}
 	}
 }; 
