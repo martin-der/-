4d800f4a85444261c93bbe61e01433ac9d2929dc
Martin Salazard <martin.der.aze@gmail.com>
2016-07-31 21:21:14 +0200

    add config defaults

---
+++ config.js
@@ -0,0 +1,28 @@
+
+
+var config = {
+	data : {
+		author : {
+			name : "",
+			email : "",
+			phone : ""
+		}
+	},
+	viewer : {
+		header : null,
+		footer : null
+	},
+	theme : {
+		defaultsTo : 'generali/pdofsi'
+	},
+	settings : {
+		log : {
+			level : 'warn',
+			theme : { level : 'info' }
+		},
+		notification : {	
+			level : 'warn',
+			theme : { level : 'info' }
+		}
+	}
+};
