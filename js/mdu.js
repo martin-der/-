8c545029b1f58d72f2886557a966bb494616acb7
Martin Salazard <martin.der.aze@gmail.com>
2016-08-04 23:16:23 +0200

    mdu : add function to get hash from URL

--- js/mdu.js
+++ js/mdu.js
@@ -161,5 +161,9 @@ MDU.getFilenameFromURL = function (url) {
 MDU.getURLParameter = function (key) {
 	return decodeURIComponent((new RegExp('[?|&]' + key + '=' + '([^&;]+?)(&|#|;|$)').exec(location.search) || [null, ''])[1].replace(/\+/g, '%20')) || null;
 };
+MDU.getHashPartFromURL = function (url) {
+	var hash_position = url.indexOf('#');
+	return hash_position < 0 ? null : url.substr(hash_position+1);
+};
 
 
